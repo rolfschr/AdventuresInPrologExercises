0da5660ed0cb13ca2a5fd9fbf3d1e08577ac193c
Rolf Schroder <rolf.schr@gmail.com>
2014-06-19 21:50:33 +0200

    add more family members

--- gene.pl
+++ gene.pl
@@ -1,7 +1,15 @@
 male(bart).
 male(homer).
+male(grandpa).
 
 female(march).
+female(lisa).
+female(maggy).
 
 parent(homer,bart).
 parent(homer,lisa).
+parent(homer,maggy).
+parent(march,bart).
+parent(march,lisa).
+parent(march,maggy).
+parent(grandpa,home).
