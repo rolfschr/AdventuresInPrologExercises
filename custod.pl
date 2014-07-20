9dcde9295762800bef12f83a1fee2bd48ac53079
Rolf Schroder <rolf.schr@gmail.com>
2014-07-20 22:05:33 +0200

    Add good_customer etc.

--- custod.pl
+++ custod.pl
@@ -1,3 +1,4 @@
+% name, city, credit-rating
 customer(tom,toronto,aaa).
 customer(nancy,newcastle,bbb).
 customer(bernd,berlin,aaa).
@@ -20,3 +21,20 @@ inventory(p4,2).
 inventory(p5,23).
 
 get_inventory(Name) :- item(Id,Name,_),inventory(Id, Amount), write(Amount).
+
+item_quantity(Name, Quantity) :- item(Id,Name,_), inventory(Id, Quantity).
+
+list_inventory :-
+	format('Inventory:'),
+	nl,
+	item_quantity(Item, Quantity),
+	tab(2),
+	format('~w: ~w', [Item, Quantity]),
+	nl,
+	fail.
+list_inventory.
+
+good_customer(X) :- customer(X,_,Rating), good_rating(Rating).
+
+good_rating(Rating) :- atom_chars(Rating, [H|_]), H = a.
+good_rating(Rating) :- atom_chars(Rating, [H|_]), H = b.
