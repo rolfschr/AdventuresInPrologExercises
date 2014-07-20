a4e0a89f8fc671224ab2f0c736afd99beba50124
Rolf Schroder <rolf.schr@gmail.com>
2014-07-20 15:55:30 +0200

    Add uncle/aunt/... predicates

--- gene.pl
+++ gene.pl
@@ -1,6 +1,7 @@
 male(bart).
 male(homer).
 male(grandpa).
+male(will).
 
 female(march).
 female(lisa).
@@ -13,9 +14,21 @@ parent(march,bart).
 parent(march,lisa).
 parent(march,maggy).
 parent(grandpa,homer).
+parent(grandpa,will).
+parent(will,joe).
 
 motherOf(X) :- parent(M, X), female(M), write(M).
 fatherOf(X) :- parent(F, X), male(F), write(F).
 
 grandmotherOf(X) :- parent(P, X), motherOf(P).
 grandfatherOf(X) :- parent(P, X), fatherOf(P).
+
+sibling(X,Y) :- parent(P,X),parent(P,Y), X \= Y.
+
+brotherOf(X, Y) :- sibling(X,Y), male(Y).
+sisterOf(X, Y) :- sibling(X,Y), female(Y).
+
+uncleOf(X,Y) :- parent(P,X),brotherOf(P,Y).
+auntOf(X,Y) :- parent(P,X),sisterOf(P,Y).
+
+cousinOf(X,Y) :- parent(P,X),sibling(P,S),parent(S,Y).
