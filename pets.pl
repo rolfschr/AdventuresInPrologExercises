ef7061c9adf573decbf269ed83b298182a2ee2b3
Rolf Schroder <rolf.schr@gmail.com>
2014-08-17 15:59:59 +0200

    Add known/3 to expert system

--- pets.pl
+++ pets.pl
@@ -6,8 +6,20 @@ pet(mouse):- size(small), noise(squeak).
 size(X):- ask(size, X).
 noise(X):- ask(noise, X).
 
+:- dynamic known/3.
+%known(Attr, Val, YesNo).
+
 ask(Attr, Val):-
-	write(Attr),tab(1),write(Val),
-	tab(1),write('(yes/no)'),write(?),
-	read(X),
-	X = yes.
+	not(known(Attr, Val, _)),
+	format('~w:~w (yes/no) ? ', [Attr, Val]),
+	read(YesNo),
+	assertz(known(Attr, Val, YesNo)),
+	ask(Attr, Val),
+	!.
+ask(Attr, Val):-
+	known(Attr, Val, yes),
+	!.
+ask(Attr, Val):-
+	known(Attr, Val, _),
+	fail,
+	!.
