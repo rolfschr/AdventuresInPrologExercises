011e6fb951d0764d8c7ca8fd856738ce11c5ff1c
Rolf Schroder <rolf.schr@gmail.com>
2014-08-31 19:57:15 +0200

    Add menuask/2 to expert system

--- pets.pl
+++ pets.pl
@@ -3,11 +3,13 @@ pet(dog):- size(medium), noise(woof).
 pet(cat):- size(medium), noise(meow).
 pet(mouse):- size(small), noise(squeak).
 
-size(X):- ask(size, X).
+size(X):- menuask(size, X, [large, medium, small]).
 noise(X):- ask(noise, X).
 
 :- dynamic known/3.
 %known(Attr, Val, YesNo).
+:- dynamic known/2.
+%known(Attr, Val).
 
 ask(Attr, Val):-
 	not(known(Attr, Val, _)),
@@ -23,3 +25,29 @@ ask(Attr, Val):-
 	known(Attr, Val, _),
 	fail,
 	!.
+
+menuask(Attr, Val, LVals) :-
+	not(known(Attr, Val, _)),
+	menu_display(Attr, LVals),
+	menu_select(LVals, Val),
+	assertz(known(Attr, Val, yes)).
+menuask(Attr, Val, _) :-
+	ask(Attr, Val).
+
+menu_display(Attr, X) :-
+	format('~w:~n', [Attr]),
+	menu_display(Attr, X, 1).
+menu_display(_, [], _).
+menu_display(Attr, [H|T], N) :-
+	format('~w) ~w~n', [N, H]),
+	Ninc is N + 1,
+	menu_display(Attr, T, Ninc).
+
+menu_select(LVals, Val) :-
+	format('Number? '),
+	read(N),
+	menu_select(N, LVals, Val).
+menu_select(1, [H|_], H).
+menu_select(N, [_|T], Select) :-
+	Ndec is N - 1,
+	menu_select(Ndec, T, Select).
